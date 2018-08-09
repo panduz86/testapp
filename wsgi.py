1dcc8b7029c68b49010904ae48d4bd67537f81b3
Paolo PANVINI <paolo.panvini@amadeus.com>
2018-08-09 16:08:07 +0200

    Added new manager

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import request
 application = Flask(__name__)
 
 
 @application.route("/")
 def hello():
     return "Hello World!"
+
+
+@application.route('/hello', methods=['GET', 'POST'])
+def better_hello():
+    nome = request.args.get('nome')
+    return "Hello " + nome + "!"
 
 
 if __name__ == "__main__":
